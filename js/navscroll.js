5683888ed38ccf37d112d59abc766c286ce4c424
mvogee <mvogee@yahoo.com>
2022-07-18 11:05:05 -0700

    navigation pop in and out on scrolling up and down and style updates.

---
+++ js/navscroll.js
@@ -0,0 +1,34 @@
+const nav = document.querySelector("nav");
+let didScroll = false;
+let lastScrollTop = 0;
+let scrollDelta = 100;
+
+const scrollAction = () => {
+    let scrollTop = window.scrollY;
+    if (Math.abs(scrollTop - lastScrollTop) <= scrollDelta) {
+        return ;
+    }
+    if (scrollTop > lastScrollTop && scrollTop > nav.scrollHeight) {
+        if (!nav.classList.contains("nav-hidden")) {
+            nav.classList.add("nav-hidden", "nav-scrolled");
+        }
+        
+    }
+    else {
+        nav.classList.remove("nav-hidden");
+        if (scrollTop - scrollDelta <= 0) {
+            nav.classList.remove("nav-scrolled");
+        }
+    }
+    lastScrollTop = scrollTop;
+};
+
+document.addEventListener('scroll', (scrollEvent) => {
+    didScroll = true;
+    setInterval(() => {
+        if (didScroll) {
+            scrollAction(scrollEvent);
+            didScroll = false;
+        }
+    }, 500);
+});
